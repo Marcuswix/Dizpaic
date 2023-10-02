477b328c63fdff57d3bcf8a527f0e16b02c308b0
Marcus <Marcuswix@yahoo.se>
2023-10-02 14:47:46 +0200

    Näst intill  med index-sidan i 1920-mode, checkbox rutan är nu klar

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+const toggleButton = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+
+toggleButton.addEventListener("click", () => {
+  menu.classList.add('open')
+  overlay.classList.add('show')
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.remove('open')
+  overlay.classList.remove('show')
+})
