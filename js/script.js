e2eeacaee9e1c41014b858e3619caa8f380065e4
Marcus <Marcuswix@yahoo.se>
2023-10-10 13:57:31 +0200

    Har lagt till en 'Till-toppen-knapp' på index-sidan

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Menu
 const toggleButton = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 const overlay = document.getElementById("overlay");
@@ -24,5 +24,35 @@ overlay.addEventListener('click', () => {
   menu.classList.remove('open')
   overlay.classList.remove('show')
 })
+
+// Go to top function
+
+function goToTop()
+{
+  window.scrollTo(
+    {
+      top: 0,
+      behavior: "smooth"
+    }
+  )
+}
+
+// Hide "Go to top"-button
+
+window.addEventListener("scroll", function()
+  {
+    let element = document.getElementById("Go-To-Top")
+
+    if (window.scrollY > 200)
+    {
+      element.classList.remove("btn-hide")
+    }
+
+    else
+    {
+      element.classList.add("btn-hide")
+    }
+  }
+)
 
 
